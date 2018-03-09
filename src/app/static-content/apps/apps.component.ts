ad9d9bc2bcf697ec7e8e41086711da60da1dd080
Tundra Work <tundrawork@gmail.com>
2018-03-10 01:35:51 +0800

    Update static pages.

--- src/app/static-content/apps/apps.component.ts
+++ src/app/static-content/apps/apps.component.ts
@@ -1,49 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
-import { Title } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'apps-guide',
-    templateUrl: './apps.html',
-    styleUrls: ['./apps.less']
+    templateUrl: './apps.html'
 })
-export class AppsComponent implements OnInit, OnDestroy {
-    private _subscription = new Subscription();
-
-    siteTitle = SITE_TITLE;
-    chromeExtensionId = CHROME_EXTENSION_ID;
-
-    showAndroid: boolean;
-
-    expanded1 = false;
-
-    expanded2 = false;
-
-    constructor(private _route: ActivatedRoute,
-                titleService: Title) {
-        titleService.setTitle(`Apps - ${SITE_TITLE}`);
-    }
-
-    ngOnInit(): void {
-        this._subscription.add(
-            this._route.params
-                .subscribe(params => {
-                    console.log(params);
-                    this.showAndroid = !!params['android'];
-                })
-        );
-    }
-
-    ngOnDestroy(): void {
-        this._subscription.unsubscribe();
-    }
-
-    expand1(): void {
-        this.expanded1 = true;
-    }
-
-    expand2(): void {
-        this.expanded2 = true;
-    }
+export class AppsComponent {
+    
 }
